7d5acd907fe7445582657fd181480e5ee7585964
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-09-12 14:31:43 -0400

    Adding 3sum

---
+++ 3sum/3Sum.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/3sum
+
+
+class Solution:
+    def threeSum(self, nums):
+        if len(nums) < 3:
+            return []
+
+        out = set()
+        nums = sorted(nums)
+        i = 0
+        while i < len(nums) - 2:
+            while i < len(nums) - 2 and nums[i] == nums[i + 1] and nums[i + 1] == nums[i + 2]:
+                if nums[i] == 0 and nums[i + 1] == 0 and nums[i + 2] == 0:
+                    out.add((0, 0, 0))
+                i += 1
+            out.update(self.twoSum(nums, i + 1, nums[i]))
+            i += 1
+        return [list(i) for i in out]
+
+    def twoSum(self, nums, ind, v):
+        out = set()
+        l, r = ind, len(nums) - 1
+
+        while l < r:
+            if v + nums[l] + nums[r] == 0:
+                out.add((v, nums[l], nums[r]))
+                l += 1
+                r -= 1
+            elif v + nums[l] + nums[r] < 0:
+                l += 1
+            else:
+                r -= 1
+
+        return out
+
+
+print(Solution().threeSum([0] * 10000))
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ![Github](https://img.shields.io/badge/languages-python-green.svg?longCache=true&style=for-the-badge)
 ![GitHub](https://img.shields.io/github/license/mashape/apistatus.svg?style=for-the-badge)
+
+| #        |  Problem      | Difficulty      | Solution |      
+|---------------- | --------------- | --------------- | -------- |
+15 | [3Sum](https://leetcode.com/problems/3sum) | Medium | [Python](3sum/3sum.py)
