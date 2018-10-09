99f18352091ee9d0d1d18a28392d707fe87906ad
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-10-09 17:38:07 -0400

    Adding remove-element

---
+++ Python/remove-element.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/remove-element
+
+
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
