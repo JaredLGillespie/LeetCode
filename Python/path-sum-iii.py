9932b0b1a2161d04fdbd122bc3aa35245a2d5a1d
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-11-30 11:40:51 -0500

    Removing path-sum-iii test cases

--- Python/path-sum-iii.py
+++ Python/path-sum-iii.py
@@ -2,11 +2,11 @@
 
 
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution(object):
     def pathSum(self, root, sum):
@@ -35,20 +35,3 @@ class Solution(object):
         total_sums = 0
         search(root, [])
         return total_sums
-
-
-def build_tree(root, index=0):
-    if root is None: return None
-    if index >= len(root): return None
-    if root[index] is None: return None
-    node = TreeNode(root[index])
-    node.left = build_tree(root, index * 2 + 1)
-    node.right = build_tree(root, index * 2 + 2)
-    return node
-
-
-root = [10,5,-3,3,2,None,11,3,-2,None,1]
-sum = 8
-
-tree = build_tree(root)
-print(Solution().pathSum(tree, sum))
