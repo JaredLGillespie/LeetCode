bda7fd62b7d3b0537a06c9b604cecb28942a3aef
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-12-02 20:31:40 -0500

    Adding contains-duplicate

---
+++ Python/contains-duplicate.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums)) != len(nums)
