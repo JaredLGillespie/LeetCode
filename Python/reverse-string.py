386d38f29aefc137b4525eb205f3821884e6f013
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-12-12 22:46:35 -0500

    Adding reverse-string

---
+++ Python/reverse-string.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/reverse-string/
+
+
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
