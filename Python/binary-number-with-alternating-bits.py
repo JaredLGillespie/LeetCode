9a0e34ff519a62ba79d4b0364ad9d0b34a767e27
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-12-21 00:38:04 -0500

    Adding max-consecutive-ones

--- Python/binary-number-with-alternating-bits.py
+++ Python/binary-number-with-alternating-bits.py
@@ -1,2 +1,18 @@
 # https://leetcode.com/problems/binary-number-with-alternating-bits/
 
+
+class Solution:
+    def hasAlternatingBits(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        prev_bit = -1
+
+        while n > 0:
+            cur_bit = n & 1
+            if cur_bit == prev_bit: return False
+            prev_bit = cur_bit
+            n >>= 1
+
+        return True
---
+++ Python/max-consecutive-ones.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/max-consecutive-ones/
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_ones = cur_ones = 0
+
+        for n in nums:
+            if n == 1: cur_ones += 1
+            else:
+                max_ones = max(max_ones, cur_ones)
+                cur_ones = 0
+
+        max_ones = max(max_ones, cur_ones)
+
+        return max_ones
