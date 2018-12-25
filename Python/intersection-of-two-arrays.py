400a58d185b5512f241324abf81a1062327d1c37
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-12-25 13:56:41 -0500

    Adding intersection-of-two-arrays

---
+++ Python/intersection-of-two-arrays.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/intersection-of-two-arrays/
+
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1).intersection(set(nums2)))
